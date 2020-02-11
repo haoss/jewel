2cd2c71576f7b1cae3a3f9b77d5d69ca00c32256
Dmitriy Khristoev <haossut@gmail.com>
2020-02-12 01:20:06 +0200

    new styles

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -51,7 +51,8 @@ $(document).on('ready', function(){
 
   $('.open-popup-link').magnificPopup({
     type: 'inline',
-    midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+    midClick: true,
+    showCloseBtn: false
   });
 
   $('.j-carousel').slick({
@@ -64,6 +65,10 @@ $(document).on('ready', function(){
     pauseOnHover: false,
     pauseOnDotsHover: false
   });
+
+  $('.modal__close').on('click', function(){
+    $.magnificPopup.close();
+  })
 
   mobileNav();
   navBtnScroll();
